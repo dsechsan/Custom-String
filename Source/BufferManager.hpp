5ea6805a85868ad5b74d261f713530bea6318d4f
sandeepchintada <sandeep.chdse@gmail.com>
2024-02-16 23:51:23 -0800

    Changing the preSize to check speed improvements on autograder

--- Source/BufferManager.hpp
+++ Source/BufferManager.hpp
@@ -11,7 +11,7 @@
 
 namespace ECE141   {
 
-template <typename T, size_t aPreSize = 12>
+template <typename T, size_t aPreSize = 32>
     class BufferManager {
     public:
         //finish me - default constructor
